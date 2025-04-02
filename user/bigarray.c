b4754e8dcda98e947ac6a2bb9d9f01afc4cf18d1
OrMalky <malky@bgu.post.ac.il>
2025-04-02 09:22:58 +0000

    fixed bigarray.c to use exit status of children instead of pipes for total sum

--- user/bigarray.c
+++ user/bigarray.c
@@ -10,7 +10,6 @@ int main(){
     int* n = malloc(sizeof(int));
     int* statuses = malloc(NPROCS * sizeof(int));
     int sum = 0;
-    int pipefd[NPROCS][2]; // Array of pipes for each child process
 
 
     // Initialize the array
@@ -18,55 +17,45 @@ int main(){
         arr[i] = i;
     }
 
-    // Create pipes
-    for (int i = 0; i < NPROCS; i++) {
-        if (pipe(pipefd[i]) < 0) {
-            printf("Pipe %d creation failed.\n", i);
-            exit(1, "error");
-        }
-    }
-
     int i = forkn(NPROCS, pids);
-    if (i == 0){
+
+    if (i == -1){
+        exit(-1,"forkn failed");
+
+    } else if (i == 0){
         // Parent process
-        for (int j = 0; j < NPROCS; j++) {
-            close(pipefd[j][1]); // Close write end of the pipe
-        }
         waitall(n, statuses);
         printf("\n");
-        int child_sum;
-        for (int j = 0; j < NPROCS; j++) {
-            read(pipefd[j][0], &child_sum, sizeof(int)); // Read sum from child
-            sum += child_sum;
-            printf("pid: %d\n", pids[j]);
+
+        if (*n != NPROCS) {
+            exit(-(*n), "waitall error");
         }
-    } else if (i==-1){
-        printf("fork operation failed.\n");
-        exit(1,"failed");
-        return 1;
+
+        for (int j = 0; j < NPROCS; j++) {
+            sum += statuses[j];
+            printf("pid: %d, sum: %d\n", pids[j], statuses[j]);
+        }
+
+        printf("Total sum: %d\n", sum);
+        exit(0,"completed");
+
     } else {
         // Child process
         int chunk = SIZE / NPROCS;
         int start = (i - 1) * chunk;
         int end = (i == NPROCS) ? SIZE : i * chunk;
-        int local_sum = 0;
-    
-        for (int j = start; j < end; j++) {
-            local_sum += arr[j];
-        } 
 
-        printf("sum=%d\n", local_sum);
-        write(pipefd[i - 1][1], &local_sum, sizeof(int)); // Write sum to pipe
-        close(pipefd[i - 1][1]); // Close write end of the pipe
-        exit(0, "");
+        for (int j = start; j < end; j++) {
+            sum += arr[j];
+        }
+
+        printf("sum=%d\n", sum);
+        exit(sum, "");
     }
-    if(*n!=NPROCS){
-        printf("fork operation failed. %d\n", *n);
-        exit(1,"failed");
-        return 1;
-    }
-    
-    printf("Total sum: %d\n", sum);
-    exit(0,"completed");
+
+    free(arr);
+    free(pids);
+    free(n);
+    free(statuses);
     return 0;
 }
